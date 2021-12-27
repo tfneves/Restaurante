b689f8b48f02d2cd0379df0804d0c1c9d4461f36
Thomaz Ferreira Neves Dos Santos <thomaz.ferreira@mercadolivre.com>
2021-12-27 20:10:43 -0300

    first commit

---
+++ meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/MeliRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.restaurante.meli_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliRestauranteApplication.class, args);
+	}
+
+}
---
+++ meli_restaurante/src/test/java/br/com/meli/restaurante/meli_restaurante/MeliRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.restaurante.meli_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
