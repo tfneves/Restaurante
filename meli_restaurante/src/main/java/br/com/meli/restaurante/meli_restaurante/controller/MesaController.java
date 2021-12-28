22c2f90af4cb8a8a33a1135eb014d5e0e28bb4b9
Francosta08 <francisco.fcosta@mercadolivre.com>
2021-12-27 21:52:23 -0300

    criado rota : /mesa

--- meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/controller/MesaController.java
+++ meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/controller/MesaController.java
@@ -1,4 +1,13 @@
 package br.com.meli.restaurante.meli_restaurante.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mesa/")
 public class MesaController {
+
+	public  String mesaController(){
+		return null;
+	}
 }
