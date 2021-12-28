efa40a4089f5887ee4d754ffa66cdafb2b3c3081
Francosta08 <francisco.fcosta@mercadolivre.com>
2021-12-27 22:02:58 -0300

    Criado rota para pedidoController - /pedido

--- meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/controller/PedidoController.java
+++ meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/controller/PedidoController.java
@@ -1,4 +1,14 @@
 package br.com.meli.restaurante.meli_restaurante.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pedido")
 public class PedidoController {
-}
+		public  String pedidoController(){
+			return null;
+		}
+	}
+
+
