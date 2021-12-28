6b40bb1eb206d4461ce6afeb1c717e87bcdeab5a
André Arroxellas <andre.arroxellas@mercadolivre.com>
2021-12-28 10:00:58 -0300

    Add CaixaService && CaixaController
    - Serviço CaixaService depende de implementação de MesaDTO para método
      fechaMesa
    - Controller CaixaController depende de MesaDTO como retorno
    
    Implementação:
    - Requerimento que Caixa fosse static (ou seja, fosse persistente e
      único na  aplicação):
    --- Singleton
    - Requerimento de separação de camadas de domínio:
    --- Builder

---
+++ meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/controller/CaixaController.java
@@ -0,0 +1,27 @@
+package br.com.meli.restaurante.meli_restaurante.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.meli.restaurante.meli_restaurante.service.CaixaService;
+
+@RestController
+@RequestMapping("/caixa}")
+public class CaixaController {
+	CaixaService caixaService = new CaixaService();
+
+	@GetMapping("/{mesaId}")
+	public ResponseEntity<MesaDTO> fechaMesa(@PathVariable int mesaId) {
+		return ResponseEntity.ok(caixaService.fechaMesa(mesaId));
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<BigDecimal> consultaValorCaixa() {
+		return ResponseEntity.ok(caixaService.getValorTotal());
+	}
+}
---
+++ meli_restaurante/src/main/java/br/com/meli/restaurante/meli_restaurante/service/CaixaService.java
@@ -0,0 +1,19 @@
+package br.com.meli.restaurante.meli_restaurante.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import br.com.meli.restaurante.meli_restaurante.dto.CaixaDTO;
+
+@Service
+public class CaixaService {
+
+	public MesaDTO fechaMesa() {
+		return null;
+	}
+
+	public BigDecimal getValorTotal() {
+		return CaixaDTO.getInstance().getValorTotal();
+	}
+}
